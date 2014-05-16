43910bf0d0d800c4e0bc1e6d69f542c17730de82
Marmik Desai <marmikdesai@live.com>
2014-05-16 20:07:37 +0530

    fix Firefox animation bug
    fix Firefox animation bug

--- demo/bootstrap_carousel_animation/responsive_slider.js
+++ demo/bootstrap_carousel_animation/responsive_slider.js
@@ -4,46 +4,58 @@
  * Further changes, comments: @Marmik Desai
  * Licensed under the MIT license
  */
- 
+
 (function($) {
-	$.fn.animat = function() {
-		img();
-		var ts = this;
-		var att;
-		this.find(".carousel-caption").children().each(function() {
-			$(this).css("opacity", 0);
-		});
-		$(".carousel-caption", this.find(".active")).children().each(function() {
-			$(this).css("opacity", 1);
-			if ($(this).data("animate")) {
-				att = $(this).data("animate");
-			}
-			$(this).removeClass(att).addClass("animated" + " " + att);
-		});
-		this.on("slid.bs.carousel", function() {
-			$(this).css("opacity", 1);
-			$(".carousel-caption", ts.find(".active")).children().each(function() {
+    $.fn.animat = function() {
+        img();
+        var ts = this;
+        var att;
+        this.find(".carousel-caption").children().each(function() {
+            $(this).css("opacity", 0);
+        });
+        $(".carousel-caption", this.find(".active")).children().each(function() {
+            $(this).css("opacity", 1);
+            if ($(this).data("animate")) {
+                att = $(this).data("animate");
+            }
+            $(this).removeClass(att).addClass("animated" + " " + att);
+        });
+        this.on("slid.bs.carousel", function() {
+			 $(".carousel-caption").each(function() {
 				if ($(this).data("animate")) {
-					att = $(this).data("animate");
-				} 
-				$(this).css("opacity", 1).removeClass(att).addClass("animated" + " " + att);
+                    att = $(this).data("animate");
+                }
+				$(this).children().removeClass(att);
+				$(this).children().css("opacity", 0);
+				$(this).children().removeClass("animated").addClass("notanimated");
 			});
-		});
-		return this;
-	};
-	function img(){
-		$(".item img").css({
-			"visibility": "hidden",
-			"display": "none"
-		});
-		var length = $(".item").length;
-		var i = 0;
-		$(".item img").each(function(i) {
-			var imag = $(this).attr('src');
-			$("#image_" + (i+1)).css({"background-image": "url(" + imag + ")",
-										"height": 500,
-										"background-size": "cover"
-									});
-		});i++;
-	}
+            $(this).css("opacity", 1);
+            $(".carousel-caption", ts.find(".active")).children().each(function() {
+                if ($(this).data("animate")) {
+                    att = $(this).data("animate");
+                }
+                $(this).css("opacity", 1).removeClass("notanimated").addClass("animated");
+                $(this).css("opacity", 1).addClass(att);
+            });
+        });
+        return this;
+    };
+
+    function img() {
+        $(".item img").css({
+            "visibility": "hidden",
+            "display": "none"
+        });
+        var length = $(".item").length;
+        var i = 0;
+        $(".item img").each(function(i) {
+            var imag = $(this).attr('src');
+            $("#image_" + (i + 1)).css({
+                "background-image": "url(" + imag + ")",
+                "height": 500,
+                "background-size": "cover"
+            });
+        });
+        i++;
+    }
 })(jQuery);
