fafefe6080a4f7d9cceb7f9581d451415a68ded1
Marmik Desai <marmikdesai@live.com>
2016-04-09 00:48:45 +0530

    v1
    v1

--- demo/offline-app/service-worker.js
+++ demo/offline-app/service-worker.js
@@ -25,6 +25,20 @@ var filesToCache = [
 	'/',
 	'index.html',
 	'styles/inline.css',
+	'styles/inline.css',
+	'images/clear.png',
+	'images/cloudy-scattered-showers.png',
+	'images/cloudy.png',
+	'images/fog.png',
+	'images/ic_add_white_24px.svg',
+	'images/ic_refresh_white_24px.svg',
+	'images/partly-cloudy.png',
+	'images/rain.png',
+	'images/scattered-showers.png',
+	'images/sleet.png',
+	'images/snow.png',
+	'images/thunderstorm.png',
+	'images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
@@ -54,7 +68,7 @@ console.log("marmik active")
 });
 
 self.addEventListener('fetch', function(e) {
-console.log("marmik fetch v1")
+console.log("marmik fetch v2")
 //  console.log('[ServiceWorker] Fetch', e.request.url);
   var dataUrl = 'https://publicdata-weather.firebaseio.com/';
   if (e.request.url.indexOf(dataUrl) === 0) {
